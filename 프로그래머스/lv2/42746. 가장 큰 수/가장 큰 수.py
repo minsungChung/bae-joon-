53bfe1003323480eb49843b1d573c90f57ed6b2d
Minsung Chung <116792686+minsungChung@users.noreply.github.com>
2023-06-22 17:02:02 +0900

    [level 2] Title: 가장 큰 수, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,11 +1,20 @@
 def solution(numbers):
     answer = ''
-    numbers = [str(n) for n in numbers]
-    numbers.sort(key=lambda x : (x*4)[:4], reverse=True)
+    tmp = []
+    for num in numbers:
+        if num < 10:
+            tmp.append([str(num)*12, 1])
+        elif num < 100:
+            tmp.append([str(num)*6, 2])
+        elif num < 1000:
+            tmp.append([str(num)*4, 3])
+        else:
+            tmp.append([str(num)*2, 4])
+            
+    tmp.sort(reverse=True)
     
-    # 0만 있을 때
-    if numbers[0] == '0':
+    for i in range(len(tmp)):
+        answer += tmp[i][0][:tmp[i][1]]
+    if int(answer) == 0:
         answer = '0'
-    else:
-        answer = ''.join(numbers)
     return answer
