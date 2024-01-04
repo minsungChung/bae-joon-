f4160d97e36a456e3b17a399fcd7de81adfbf802
Minsung Chung <116792686+minsungChung@users.noreply.github.com>
2024-01-04 13:29:40 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 248 ms, Memory: 49208 KB -BaekjoonHub

---
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.py
@@ -0,0 +1,19 @@
+import sys
+
+input = sys.stdin.readline
+
+if __name__ == '__main__':
+    n, m = map(int, input().rstrip().split())
+    dic = {}
+    for _ in range(n):
+        key, value = input().rstrip().split()
+        dic[key] = value
+
+    for _ in range(m):
+        site = input().rstrip()
+        print(dic[site])
+
+
+
+
+
