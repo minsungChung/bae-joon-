c57f192cd09dc344171cde9600ac239c983b8f65
Minsung Chung <116792686+minsungChung@users.noreply.github.com>
2024-01-13 11:21:58 +0900

    [Gold IV] Title: 네트워크 연결, Time: 204 ms, Memory: 47804 KB -BaekjoonHub

---
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.py
@@ -0,0 +1,38 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input().rstrip())
+m = int(input().rstrip())
+edges = []
+parent = list(range(n+1))
+num_edge = 0
+answer = 0
+
+for _ in range(m):
+    edges.append(tuple(map(int, input().rstrip().split())))
+
+edges.sort(key=lambda x: x[2])
+
+
+def find(x):
+    if parent[x] == x:
+        return x
+    parent[x] = find(parent[x])
+    return parent[x]
+
+def addEdge(x, y):
+    root_x = find(x)
+    root_y = find(y)
+    parent[root_x] = root_y
+
+
+
+for idx, adj, cost in edges:
+    if find(idx) != find(adj):
+        addEdge(idx, adj)
+        answer += cost
+        num_edge += 1
+        if num_edge == n-1:
+            break
+
+print(answer)
