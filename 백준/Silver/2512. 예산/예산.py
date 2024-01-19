7ff59515a76ebae13d13c5d36c680d19f3b992c2
Minsung Chung <116792686+minsungChung@users.noreply.github.com>
2024-01-19 09:40:59 +0900

    [Silver II] Title: 예산, Time: 44 ms, Memory: 32140 KB -BaekjoonHub

--- 백준/Silver/2512. 예산/예산.py
+++ 백준/Silver/2512. 예산/예산.py
@@ -1,32 +1,17 @@
+import sys
+input = sys.stdin.readline
 
-if __name__ == '__main__':
-    n = int(input())
-    moneys = list(map(int, input().split()))
-    total = int(input())
-    if total >= sum(moneys):
-        print(max(moneys))
+N = int(input())
+nums = list(map(int, input().split()))
+nums.sort()
+_max = int(input())
+
+for i in range(N):
+    base = _max // (N-i)
+    if nums[i] <= base:
+        _max -= nums[i]
     else:
-        moneys.sort()
-        cnt = n
-        while True:
-            check = True
-            avg = total / cnt
-            for i in range(n-cnt, n):
-                if moneys[i] < avg:
-                    total -= moneys[i]
-                    cnt -= 1
-                    check = False
-
-            if check:
-                print(int(avg))
-                break
-
-
-
-
-
-
-
-
-
-
+        print(base)
+        break
+else:
+    print(nums[-1])
