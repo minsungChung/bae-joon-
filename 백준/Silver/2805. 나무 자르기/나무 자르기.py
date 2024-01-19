78343a2f58cb63bc4edae7f0c22c3acd081f7c88
Minsung Chung <116792686+minsungChung@users.noreply.github.com>
2024-01-19 12:26:38 +0900

    [Silver II] Title: 나무 자르기, Time: 4640 ms, Memory: 143264 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -4,7 +4,6 @@ input = sys.stdin.readline
 N, M = map(int, input().split())
 trees = list(map(int, input().split()))
 left, right = 0, 2000000000
-answer = 0
 
 while left <= right:
     mid = (left+right) // 2
@@ -13,18 +12,14 @@ while left <= right:
     for tree in trees:
         if tree - mid > 0:
             bring += tree-mid
-        if bring > M:
+        if bring >= M:
             break
 
     else:
-        if bring == M:
-            answer = mid
-            break
         right = mid-1
         continue
 
-    answer = mid
     left = mid+1
 
 
-print(answer)
+print(right)
