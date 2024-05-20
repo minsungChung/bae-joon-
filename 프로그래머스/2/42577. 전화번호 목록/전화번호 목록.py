e2a3651d6c103e038ab5d96803728c29fbf228ab
Minsung Chung <116792686+minsungChung@users.noreply.github.com>
2024-05-20 21:02:09 +0900

    [level 2] Title: 전화번호 목록, Time: 85.11 ms, Memory: 28 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,9 +1,15 @@
 def solution(phone_book):
     answer = True
+    
+    # phone_book 정렬
     phone_book.sort()
+    
     for i in range(len(phone_book)-1):
-        if phone_book[i] == phone_book[i+1][:len(phone_book[i])]:
+        pre = phone_book[i]
+        
+        # 앞 번호가 뒷번호의 앞과 같다면
+        if phone_book[i+1][:len(pre)] == pre:
             answer = False
             break
-            
+    
     return answer
