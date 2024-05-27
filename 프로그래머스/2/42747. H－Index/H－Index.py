489faf41ac487db3fc851efad40f8eb7cf861955
Minsung Chung <116792686+minsungChung@users.noreply.github.com>
2024-05-27 20:56:46 +0900

    [level 2] Title: H-Index, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,17 +1,11 @@
 def solution(citations):
     answer = 0
     citations.sort(reverse=True)
-    base = min(len(citations), citations[0])
     
-    for i in range(base, 2, -1):
-        cnt = 0
-        for c in citations:
-            if c >= i:
-                cnt += 1
-            else:
-                break
-        if cnt >= i:
-            answer = i
+    for i in range(len(citations)):
+        if citations[i] >= i+1:
+            answer = i+1
+        else:
             break
-    
+            
     return answer
